8efefdd00d5530616076d3086c710b95f4fa17b2
jennifer j <jenniferjuarezjj@gmail.com>
2023-11-10 23:42:25 -0500

    initial commit

---
+++ src/VehicleTest.java
@@ -0,0 +1,25 @@
+
+
+public class VehicleTest {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+
+    class Vehicle {
+        String brand;
+
+        Vehicle(String brand) {
+            this.brand = brand;
+        }
+
+        void startEngine() {
+            System.out.println("Generic engine start");
+        }
+    }
+
+
+
+
+
+
+}
